fa9f6674dc66cec266c97738c740e14c1867fbc1
Zbigniew Curylo <zcurylo@localhost.localdomain>
2015-08-13 16:07:17 +0200

    changes to make it compilable

---
+++ bin/OnvifSDK-b/CMakeFiles/OnvifSDK.dir/DependInfo.cmake
@@ -0,0 +1,74 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/zcurylo/Documents/WORK_PROJECTS/OpenONVIF/OnvifSDK/Eventing/EventingClient.cpp" "/home/zcurylo/Documents/WORK_PROJECTS/OpenONVIF/bin/OnvifSDK-b/CMakeFiles/OnvifSDK.dir/Eventing/EventingClient.cpp.o"
+  "/home/zcurylo/Documents/WORK_PROJECTS/OpenONVIF/OnvifSDK/Eventing/EventingServiceImpl.cpp" "/home/zcurylo/Documents/WORK_PROJECTS/OpenONVIF/bin/OnvifSDK-b/CMakeFiles/OnvifSDK.dir/Eventing/EventingServiceImpl.cpp.o"
+  "/home/zcurylo/Documents/WORK_PROJECTS/OpenONVIF/OnvifSDK/Eventing/EventingTypes.cpp" "/home/zcurylo/Documents/WORK_PROJECTS/OpenONVIF/bin/OnvifSDK-b/CMakeFiles/OnvifSDK.dir/Eventing/EventingTypes.cpp.o"
+  "/home/zcurylo/Documents/WORK_PROJECTS/OpenONVIF/OnvifSDK/Eventing/NotificationConsumer.cpp" "/home/zcurylo/Documents/WORK_PROJECTS/OpenONVIF/bin/OnvifSDK-b/CMakeFiles/OnvifSDK.dir/Eventing/NotificationConsumer.cpp.o"
+  "/home/zcurylo/Documents/WORK_PROJECTS/OpenONVIF/OnvifSDK/Eventing/NotificationProducer.cpp" "/home/zcurylo/Documents/WORK_PROJECTS/OpenONVIF/bin/OnvifSDK-b/CMakeFiles/OnvifSDK.dir/Eventing/NotificationProducer.cpp.o"
+  "/home/zcurylo/Documents/WORK_PROJECTS/OpenONVIF/OnvifSDK/WsDiscovery/discoverable.cpp" "/home/zcurylo/Documents/WORK_PROJECTS/OpenONVIF/bin/OnvifSDK-b/CMakeFiles/OnvifSDK.dir/WsDiscovery/discoverable.cpp.o"
+  "/home/zcurylo/Documents/WORK_PROJECTS/OpenONVIF/OnvifSDK/WsDiscovery/discoverer.cpp" "/home/zcurylo/Documents/WORK_PROJECTS/OpenONVIF/bin/OnvifSDK-b/CMakeFiles/OnvifSDK.dir/WsDiscovery/discoverer.cpp.o"
+  "/home/zcurylo/Documents/WORK_PROJECTS/OpenONVIF/OnvifSDK/WsDiscovery/onvifxx.cpp" "/home/zcurylo/Documents/WORK_PROJECTS/OpenONVIF/bin/OnvifSDK-b/CMakeFiles/OnvifSDK.dir/WsDiscovery/onvifxx.cpp.o"
+  "/home/zcurylo/Documents/WORK_PROJECTS/OpenONVIF/OnvifSDK/WsDiscovery/remotediscovery_proxy.cpp" "/home/zcurylo/Documents/WORK_PROJECTS/OpenONVIF/bin/OnvifSDK-b/CMakeFiles/OnvifSDK.dir/WsDiscovery/remotediscovery_proxy.cpp.o"
+  "/home/zcurylo/Documents/WORK_PROJECTS/OpenONVIF/OnvifSDK/WsDiscovery/remotediscovery_service.cpp" "/home/zcurylo/Documents/WORK_PROJECTS/OpenONVIF/bin/OnvifSDK-b/CMakeFiles/OnvifSDK.dir/WsDiscovery/remotediscovery_service.cpp.o"
+  "/home/zcurylo/Documents/WORK_PROJECTS/OpenONVIF/OnvifSDK/WsDiscovery/wsa.cpp" "/home/zcurylo/Documents/WORK_PROJECTS/OpenONVIF/bin/OnvifSDK-b/CMakeFiles/OnvifSDK.dir/WsDiscovery/wsa.cpp.o"
+  "/home/zcurylo/Documents/WORK_PROJECTS/OpenONVIF/OnvifSDK/gen/source/stdsoap2.cpp" "/home/zcurylo/Documents/WORK_PROJECTS/OpenONVIF/bin/OnvifSDK-b/CMakeFiles/OnvifSDK.dir/gen/source/stdsoap2.cpp.o"
+  "/home/zcurylo/Documents/WORK_PROJECTS/OpenONVIF/common/helpers.cpp" "/home/zcurylo/Documents/WORK_PROJECTS/OpenONVIF/bin/OnvifSDK-b/CMakeFiles/OnvifSDK.dir/home/zcurylo/Documents/WORK_PROJECTS/OpenONVIF/common/helpers.cpp.o"
+  "/home/zcurylo/Documents/WORK_PROJECTS/OpenONVIF/OnvifSDK/source/AnalyticsServiceImpl.cpp" "/home/zcurylo/Documents/WORK_PROJECTS/OpenONVIF/bin/OnvifSDK-b/CMakeFiles/OnvifSDK.dir/source/AnalyticsServiceImpl.cpp.o"
+  "/home/zcurylo/Documents/WORK_PROJECTS/OpenONVIF/OnvifSDK/source/AnalyticsTypes.cpp" "/home/zcurylo/Documents/WORK_PROJECTS/OpenONVIF/bin/OnvifSDK-b/CMakeFiles/OnvifSDK.dir/source/AnalyticsTypes.cpp.o"
+  "/home/zcurylo/Documents/WORK_PROJECTS/OpenONVIF/OnvifSDK/source/BaseClient.cpp" "/home/zcurylo/Documents/WORK_PROJECTS/OpenONVIF/bin/OnvifSDK-b/CMakeFiles/OnvifSDK.dir/source/BaseClient.cpp.o"
+  "/home/zcurylo/Documents/WORK_PROJECTS/OpenONVIF/OnvifSDK/source/BaseServer.cpp" "/home/zcurylo/Documents/WORK_PROJECTS/OpenONVIF/bin/OnvifSDK-b/CMakeFiles/OnvifSDK.dir/source/BaseServer.cpp.o"
+  "/home/zcurylo/Documents/WORK_PROJECTS/OpenONVIF/OnvifSDK/source/DeviceClient.cpp" "/home/zcurylo/Documents/WORK_PROJECTS/OpenONVIF/bin/OnvifSDK-b/CMakeFiles/OnvifSDK.dir/source/DeviceClient.cpp.o"
+  "/home/zcurylo/Documents/WORK_PROJECTS/OpenONVIF/OnvifSDK/source/DeviceIOClient.cpp" "/home/zcurylo/Documents/WORK_PROJECTS/OpenONVIF/bin/OnvifSDK-b/CMakeFiles/OnvifSDK.dir/source/DeviceIOClient.cpp.o"
+  "/home/zcurylo/Documents/WORK_PROJECTS/OpenONVIF/OnvifSDK/source/DeviceIOServiceImpl.cpp" "/home/zcurylo/Documents/WORK_PROJECTS/OpenONVIF/bin/OnvifSDK-b/CMakeFiles/OnvifSDK.dir/source/DeviceIOServiceImpl.cpp.o"
+  "/home/zcurylo/Documents/WORK_PROJECTS/OpenONVIF/OnvifSDK/source/DeviceIOTypes.cpp" "/home/zcurylo/Documents/WORK_PROJECTS/OpenONVIF/bin/OnvifSDK-b/CMakeFiles/OnvifSDK.dir/source/DeviceIOTypes.cpp.o"
+  "/home/zcurylo/Documents/WORK_PROJECTS/OpenONVIF/OnvifSDK/source/DeviceInfoStorage.cpp" "/home/zcurylo/Documents/WORK_PROJECTS/OpenONVIF/bin/OnvifSDK-b/CMakeFiles/OnvifSDK.dir/source/DeviceInfoStorage.cpp.o"
+  "/home/zcurylo/Documents/WORK_PROJECTS/OpenONVIF/OnvifSDK/source/DeviceServiceImpl.cpp" "/home/zcurylo/Documents/WORK_PROJECTS/OpenONVIF/bin/OnvifSDK-b/CMakeFiles/OnvifSDK.dir/source/DeviceServiceImpl.cpp.o"
+  "/home/zcurylo/Documents/WORK_PROJECTS/OpenONVIF/OnvifSDK/source/DeviceTypes.cpp" "/home/zcurylo/Documents/WORK_PROJECTS/OpenONVIF/bin/OnvifSDK-b/CMakeFiles/OnvifSDK.dir/source/DeviceTypes.cpp.o"
+  "/home/zcurylo/Documents/WORK_PROJECTS/OpenONVIF/OnvifSDK/source/DisplayClient.cpp" "/home/zcurylo/Documents/WORK_PROJECTS/OpenONVIF/bin/OnvifSDK-b/CMakeFiles/OnvifSDK.dir/source/DisplayClient.cpp.o"
+  "/home/zcurylo/Documents/WORK_PROJECTS/OpenONVIF/OnvifSDK/source/DisplayServiceImpl.cpp" "/home/zcurylo/Documents/WORK_PROJECTS/OpenONVIF/bin/OnvifSDK-b/CMakeFiles/OnvifSDK.dir/source/DisplayServiceImpl.cpp.o"
+  "/home/zcurylo/Documents/WORK_PROJECTS/OpenONVIF/OnvifSDK/source/DisplayTypes.cpp" "/home/zcurylo/Documents/WORK_PROJECTS/OpenONVIF/bin/OnvifSDK-b/CMakeFiles/OnvifSDK.dir/source/DisplayTypes.cpp.o"
+  "/home/zcurylo/Documents/WORK_PROJECTS/OpenONVIF/OnvifSDK/source/MediaServiceImpl.cpp" "/home/zcurylo/Documents/WORK_PROJECTS/OpenONVIF/bin/OnvifSDK-b/CMakeFiles/OnvifSDK.dir/source/MediaServiceImpl.cpp.o"
+  "/home/zcurylo/Documents/WORK_PROJECTS/OpenONVIF/OnvifSDK/source/MediaTypes.cpp" "/home/zcurylo/Documents/WORK_PROJECTS/OpenONVIF/bin/OnvifSDK-b/CMakeFiles/OnvifSDK.dir/source/MediaTypes.cpp.o"
+  "/home/zcurylo/Documents/WORK_PROJECTS/OpenONVIF/OnvifSDK/source/ReceiverClient.cpp" "/home/zcurylo/Documents/WORK_PROJECTS/OpenONVIF/bin/OnvifSDK-b/CMakeFiles/OnvifSDK.dir/source/ReceiverClient.cpp.o"
+  "/home/zcurylo/Documents/WORK_PROJECTS/OpenONVIF/OnvifSDK/source/ReceiverServiceImpl.cpp" "/home/zcurylo/Documents/WORK_PROJECTS/OpenONVIF/bin/OnvifSDK-b/CMakeFiles/OnvifSDK.dir/source/ReceiverServiceImpl.cpp.o"
+  "/home/zcurylo/Documents/WORK_PROJECTS/OpenONVIF/OnvifSDK/source/ReceiverTypes.cpp" "/home/zcurylo/Documents/WORK_PROJECTS/OpenONVIF/bin/OnvifSDK-b/CMakeFiles/OnvifSDK.dir/source/ReceiverTypes.cpp.o"
+  "/home/zcurylo/Documents/WORK_PROJECTS/OpenONVIF/OnvifSDK/source/RecordingClient.cpp" "/home/zcurylo/Documents/WORK_PROJECTS/OpenONVIF/bin/OnvifSDK-b/CMakeFiles/OnvifSDK.dir/source/RecordingClient.cpp.o"
+  "/home/zcurylo/Documents/WORK_PROJECTS/OpenONVIF/OnvifSDK/source/RecordingServiceImpl.cpp" "/home/zcurylo/Documents/WORK_PROJECTS/OpenONVIF/bin/OnvifSDK-b/CMakeFiles/OnvifSDK.dir/source/RecordingServiceImpl.cpp.o"
+  "/home/zcurylo/Documents/WORK_PROJECTS/OpenONVIF/OnvifSDK/source/RecordingTypes.cpp" "/home/zcurylo/Documents/WORK_PROJECTS/OpenONVIF/bin/OnvifSDK-b/CMakeFiles/OnvifSDK.dir/source/RecordingTypes.cpp.o"
+  "/home/zcurylo/Documents/WORK_PROJECTS/OpenONVIF/OnvifSDK/source/ReplayServiceImpl.cpp" "/home/zcurylo/Documents/WORK_PROJECTS/OpenONVIF/bin/OnvifSDK-b/CMakeFiles/OnvifSDK.dir/source/ReplayServiceImpl.cpp.o"
+  "/home/zcurylo/Documents/WORK_PROJECTS/OpenONVIF/OnvifSDK/source/ReplayTypes.cpp" "/home/zcurylo/Documents/WORK_PROJECTS/OpenONVIF/bin/OnvifSDK-b/CMakeFiles/OnvifSDK.dir/source/ReplayTypes.cpp.o"
+  "/home/zcurylo/Documents/WORK_PROJECTS/OpenONVIF/OnvifSDK/source/SearchServiceImpl.cpp" "/home/zcurylo/Documents/WORK_PROJECTS/OpenONVIF/bin/OnvifSDK-b/CMakeFiles/OnvifSDK.dir/source/SearchServiceImpl.cpp.o"
+  "/home/zcurylo/Documents/WORK_PROJECTS/OpenONVIF/OnvifSDK/source/SearchTypes.cpp" "/home/zcurylo/Documents/WORK_PROJECTS/OpenONVIF/bin/OnvifSDK-b/CMakeFiles/OnvifSDK.dir/source/SearchTypes.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS
+  "SIGRLOG_CURRLEVEL=4"
+  "SIGRLOG_FILENAME=\"oxxServerLog.ini\""
+  "SIGRLOG_OUTPUT=1"
+  "WITH_NONAMESPACES"
+  "WITH_PURE_VIRTUAL"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "/home/zcurylo/Documents/WORK_PROJECTS/OpenONVIF/OnvifSDK/../common"
+  "/home/zcurylo/Documents/WORK_PROJECTS/OpenONVIF/OnvifSDK/gen/include"
+  "/home/zcurylo/Documents/WORK_PROJECTS/OpenONVIF/OnvifSDK/include"
+  "/home/zcurylo/Documents/WORK_PROJECTS/OpenONVIF/OnvifSDK/Eventing"
+  "/home/zcurylo/Documents/WORK_PROJECTS/OpenONVIF/OnvifSDK/WsDiscovery"
+  "src"
+  "."
+  )
+set(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+set(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+set(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
---
+++ bin/OnvifSDK-b/xml/CMakeFiles/OnvifSDK_xml.dir/cmake_clean.cmake
@@ -0,0 +1,5 @@
+
+# Per-language clean rules from dependency scanning.
+foreach(lang)
+  include(CMakeFiles/OnvifSDK_xml.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
